96a6d565a9ffc9e742872ab329bd6e226ab83fc6
Louis1407 <louis1407@outlook.com>
2018-10-25 15:43:47 +0100

    push2

---
+++ src/lwjglTest/Test.java
@@ -0,0 +1,19 @@
+package lwjglTest;
+
+import org.lwjgl.Version;
+
+import org.lwjgl.lwjgl-opengl;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Version.getVersion());
+		
+		
+		
+		
+		
+	}
+
+}
